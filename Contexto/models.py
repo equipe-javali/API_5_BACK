c3f66a6c880f764bb3a6bc83d5de364cce37d9f3
Marcus Betti <marcusbetti@gmail.com>
2025-03-27 00:57:42 -0300

    fix - inclusao do campo Resposta ao modelo Contexto

--- Contexto/models.py
+++ Contexto/models.py
@@ -4,5 +4,5 @@ from Agente.models import Agente
 
 class Contexto(models.Model):
     pergunta = models.TextField(null=False, blank=False)
-    pergunta = models.TextField(null=False, blank=False)
+    resposta = models.TextField(null=False, blank=False)
     Agente_id = models.ForeignKey(Agente, on_delete=models.PROTECT, null=False, blank=False)
