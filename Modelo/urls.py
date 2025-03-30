f7082b131b4aaa03a63f33c9a981ca83f919eab3
Marcus Betti <marcusbetti@gmail.com>
2025-03-30 16:46:12 -0300

    fix - ajustes no endpoint de listar modelo treinado

--- Modelo/urls.py
+++ Modelo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_trained_models
+from .views import list_trained_models, listar_modelos_completo
 
 urlpatterns = [
     path('modelo/listar', list_trained_models, name='list-trained-models'),
+    path('modelo/listar-completo', listar_modelos_completo, name='listar_modelos_completo'),
 ]
