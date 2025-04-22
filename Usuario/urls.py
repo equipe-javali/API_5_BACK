0c47f4cdfeaedf953f8dd5ef2976835cc48ca92c
BrunoSerpa <serpabruno2005@gmail.com>
2025-04-22 08:40:55 -0300

    140 - Adicionei a função de deletar usuários

--- Usuario/urls.py
+++ Usuario/urls.py
@@ -4,6 +4,7 @@ from .views import (
     UsuarioUpdateView,
     AdminCreateView,
     login,
+    delete_user,
     listar_usuarios_todos
 )
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path("usuario/cadastrar", UsuarioCreateView.as_view(), name="usuario-cadastrar"),
     path('admin/cadastrar', AdminCreateView.as_view(), name='admin-cadastrar'),
     path("usuario/atualizar/<int:pk>", UsuarioUpdateView.as_view(), name="usuario-atualizar"),
+    path("usuario/excluir/<int:pk>", delete_user, name="usuario-excluir"),
     path("usuario/login", login, name="usuario-login"),
     path("usuario/listagem-todos", listar_usuarios_todos, name="usuario-listagem-todos"),
 ]
