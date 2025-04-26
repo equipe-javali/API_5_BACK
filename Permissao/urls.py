a3c068e1a0e946ced540ad58d0135ec8cf4018c0
Katiane <katiane.soares@fatec.sp.gov.br>
2025-04-26 16:39:35 -0300

    fix: ajuste nas permissoes para cadastrar bot

--- Permissao/urls.py
+++ Permissao/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PermissaoCreateView
+from .views import PermissaoCreateView, buscar_permissao
 
 urlpatterns = [
     path("permissao/cadastrar", PermissaoCreateView.as_view(), name="permissao-cadastrar"),
+    path("permissao/buscar", buscar_permissao, name="permissao-buscar"),
 ]
--- Permissao/views.py
+++ Permissao/views.py
@@ -1,7 +1,24 @@
 from rest_framework import generics
 from .models import Permissao
 from .serializers import PermissaoSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 class PermissaoCreateView(generics.CreateAPIView):
     queryset = Permissao.objects.all()
     serializer_class = PermissaoSerializer
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def buscar_permissao(request):
+    """
+    Busca permissões pelo nome
+    """
+    nome = request.GET.get('nome')
+    if not nome:
+        return Response({"error": "Nome não fornecido"}, status=400)
+    
+    permissoes = Permissao.objects.filter(nome=nome)
+    serializer = PermissaoSerializer(permissoes, many=True)
+    return Response(serializer.data)
