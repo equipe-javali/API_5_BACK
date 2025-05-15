9a66a5b59a95ef8e31906cffff14a1c69238abc7
KatianeFatec <katiane.soares@fatec.sp.gov.br>
2025-05-15 14:13:43 -0300

    - Adiciona endpoint /chat/historico/ compatível com o front

--- Chat/urls.py
+++ Chat/urls.py
@@ -3,7 +3,8 @@ from .views import (
     chat_view,
     chat_enviar_mensagem,
     iniciar_conversa, 
-    listar_chatbots,       
+    listar_chatbots,
+    obter_historico_chat,       
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path("chat/enviar-mensagem", chat_enviar_mensagem, name="chat-enviar-mensagem"),
     path("chat/iniciar-conversa", iniciar_conversa, name="chat-iniciar-conversa"),  # New route
     path("chat/listar", listar_chatbots, name="listar-chatbots"),
+     path('historico/<int:chat_id>', obter_historico_chat, name='historico'),
 ]
